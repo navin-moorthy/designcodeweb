b216305a86d088b27a9e67bbbe36307e9016668f
Navin Moorthy <navin007.a@gmail.com>
2021-08-07 20:15:23 +0530

    feat(styles): ✨ add text, color & global styles

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,5 +1,7 @@
 import React from "react"
 import styled from "styled-components"
+import { themes } from "../styles/ColorStyles"
+import { H1, MediumText } from "../styles/TextStyles"
 
 function HeroSection() {
   return (
@@ -38,13 +40,8 @@ const TextWrapper = styled.div`
   gap: 30px;
 `
 
-const Title = styled.h1`
-  font-weight: bold;
-  font-size: 60px;
-  color: white;
+const Title = styled(H1)`
+  color: ${themes.dark.text1};
 `
 
-const Description = styled.p`
-  font-size: 17px;
-  line-height: 130%;
-`
+const Description = styled(MediumText)``
